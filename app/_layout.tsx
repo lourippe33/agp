f3f9a0c7b285d8d2ef50274e92e3833a05ecdc8a
Eric <lourippe33@gmail.com>
2025-08-21 17:07:50 +0200

    Restaurer le layout complet

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,13 +14,11 @@ import {
   Poppins_600SemiBold,
   Poppins_700Bold,
 } from '@expo-google-fonts/poppins';
-import * as SplashScreen from 'expo-splash-screen'
-import { useFrameworkReady } from '@/hooks/useFrameworkReady';
+import * as SplashScreen from 'expo-splash-screen';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  useFrameworkReady();
   const [fontsLoaded, fontError] = useFonts({
     'Inter-Regular': Inter_400Regular,
     'Inter-Medium': Inter_500Medium,
