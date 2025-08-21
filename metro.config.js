d7327118cb6f3ead04086400ff412dc9a17f7a33
Eric <lourippe33@gmail.com>
2025-08-21 20:18:20 +0200

    Updated package-lock.json

--- metro.config.js
+++ metro.config.js
@@ -13,10 +13,14 @@ config.resolver.alias = {
 
 // Improve SVG compatibility on web
 config.resolver.resolverMainFields = ['react-native', 'browser', 'main'];
-config.resolver.alias = {
-  ...config.resolver.alias,
-  'react-native-svg': 'react-native-svg/lib/module/ReactNativeSVG.web.js',
-};
+
+// Enhanced SVG web compatibility
+if (process.env.EXPO_PLATFORM === 'web') {
+  config.resolver.alias = {
+    ...config.resolver.alias,
+    'react-native-svg': 'react-native-svg/lib/module/ReactNativeSVG.web.js',
+  };
+}
 
 // Reset cache on start
 config.resetCache = true;
