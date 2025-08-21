94483b6a9d8916f74aa00f08f1c10bb9a20bb88f
Eric <lourippe33@gmail.com>
2025-08-21 17:03:42 +0200

    Supprimer le hook manquant

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,10 +15,8 @@ import {
   Poppins_700Bold,
 } from '@expo-google-fonts/poppins';
 import * as SplashScreen from 'expo-splash-screen'
-import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function RootLayout() {
-  useFrameworkReady();
   const [fontsLoaded, fontError] = useFonts({
     'Inter-Regular': Inter_400Regular,
     'Inter-Medium': Inter_500Medium,
