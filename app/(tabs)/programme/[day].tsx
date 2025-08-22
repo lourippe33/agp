58e7e832733a784c7c67e0f983da1b1f8ac66880
Eric <lourippe33@gmail.com>
2025-08-22 15:44:45 +0200

    Corriger le déclenchement du bouton de sauvegarde

--- app/(tabs)/programme/[day].tsx
+++ app/(tabs)/programme/[day].tsx
@@ -89,6 +89,7 @@ export default function DayProgramScreen() {
     };
     
     setCompletedActions(newCompleted);
+    setHasUnsavedChanges(true);
     
     try {
       await AsyncStorage.setItem(`day_${dayNumber}_completed`, JSON.stringify(newCompleted));
